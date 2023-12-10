9f52ab0acf12d34d8c10345bea4f4faf6bb4d655
Van Ly Hai <lyhai1502@gmail.com>
2023-12-10 23:57:57 +0700

    clean code send mail

--- Config/Static.java
+++ Config/Static.java
@@ -3,15 +3,18 @@ package Config;
 /**
  * Static
  */
+
 public class Static {
     public static String SERVER = "127.0.0.1";
     public static int STMP_PORT = 2225;
     public static int POP3_PORT = 3335;
     public static String PASSWORD = "123456";
-    public static String USERNAME = "LYHAI";
+    public static String USERNAME = "Van Ly Hai";
     public static String SENDER = "lyhai1502@gmail.com";
     public static int AUTOLOAD = 10;
-    
+
+    public static String BOUNDARY = "------------" + System.currentTimeMillis();
+
     Static(String server, int stmp_port, int pop3_port, String password, String username, String sender, int autoload) {
         SERVER = server;
         STMP_PORT = stmp_port;
--- Main.java
+++ Main.java
@@ -4,7 +4,14 @@ import SendMail.SendMail;
 
 public class Main {
     public static void main(String[] args) throws UnknownHostException, IOException {
-       SendMail sendMail = new SendMail();
-       sendMail.sendMail();
+
+        String[] recipients = { "lyhai1502@gmail.com" };
+        String[] ccRecipients = { "lyhai1502.work@gmail.com" };
+        String[] bccRecipients = {};
+        String subject = "Test2";
+        String body = "Test2";
+        String[] attachmentFilePaths = { "/Users/vanlyhai/Downloads/doc.docx" };
+        SendMail sendMail = new SendMail(recipients, ccRecipients, bccRecipients, subject, body, attachmentFilePaths);
+        sendMail.sendMail();
     }
 }
