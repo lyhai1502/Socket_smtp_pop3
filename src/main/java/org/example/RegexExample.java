d0e1b60ebff330427fe9a2a11d623f7cba806955
ngoxuanchien <74176040+ngoxuanchien@users.noreply.github.com>
2023-12-11 17:07:31 +0700

    receive email

---
+++ src/main/java/org/example/RegexExample.java
@@ -0,0 +1,26 @@
+package org.example;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class RegexExample {
+
+    public static void main(String[] args) {
+        String input = "This is the first block.\n\n" +
+                "This is the second block.\n\n" +
+                "And this is the third block.";
+
+        // Biểu thức chính quy để tìm kiếm các chuỗi giữa hai dấu xuống dòng
+        String regex = "(?s)\n\n(.*?)\n\n";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(input);
+
+        int blockNumber = 1;
+
+        while (matcher.find()) {
+            String extractedText = matcher.group(1);
+            System.out.println("Block " + blockNumber + ":\n" + extractedText.trim());
+            blockNumber++;
+        }
+    }
+}
+
