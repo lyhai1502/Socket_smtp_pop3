0e35852160c68353d7d7932659982614c6c37cd9
Van Ly Hai <lyhai1502@gmail.com>
2023-12-12 00:00:52 +0700

    combine smtp pop3

---
+++ src/main/java/com/example/ReceiveMail/MySocket.java
@@ -0,0 +1,16 @@
+package com.example.ReceiveMail;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class MySocket {
+    private String server = "127.0.0.1";
+    private Integer port = 3335;
+
+    void receiveEmail() {
+
+    }
+}
